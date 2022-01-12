1abe34264aacf790b0ea21da7728310fa8b81147
rarahata <rei.arahata@gmail.com>
2022-01-12 12:35:51 +0000

    add html structure

--- src/weather.js
+++ src/weather.js
@@ -4,7 +4,35 @@ export default function Weather(){
     return (
         <div className="Weather">
         <button className='btn btn-primary'>Hello</button>
-        <p>Hello from Weather</p>
+        <p>Monday, 27th April</p>
+        <h2>6:30am</h2>
+        <p>London</p>
+        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon"></img>
+        <h1>10 °C</h1>
+        <p>Monday</p>
+        <hr></hr>
+        <div className="row">
+        <div className="col-3">
+            <p>Tue</p>
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon"></img>
+            <p>12°C</p>
+        </div>
+        <div className="col-3">
+            <p>Tue</p>
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon"></img>
+            <p>12°C</p>
+        </div>
+        <div className="col-3">
+            <p>Tue</p>
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon"></img>
+            <p>12°C</p>
+        </div>
+        <div className="col-3">
+            <p>Tue</p>
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon"></img>
+            <p>12°C</p>
+        </div>
+        </div>
         </div>
         );
 }
