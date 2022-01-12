6f642835763e84f101632eeb8e7c59d639a2a0b5
rarahata <rei.arahata@gmail.com>
2022-01-12 12:23:24 +0000

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Weather from "./weather.js";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <div className='container'>
+      <Weather />
+    </div>
     </div>
   );
 }
---
+++ src/weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Weather(){
+    return (
+        <div className="Weather">
+        <button className='btn btn-primary'>Hello</button>
+        <p>Hello from Weather</p>
+        </div>
+        );
+}
