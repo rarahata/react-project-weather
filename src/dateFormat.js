fb961674f0ddf63007ecaf0d11b0886dc0c9e4f5
rarahata <rei.arahata@gmail.com>
2022-01-23 17:59:06 +0000

    install date fn

--- src/dateFormat.js
+++ src/dateFormat.js
@@ -1,16 +1,20 @@
 import React from "react";
+import {format} from "date-fns";
 
 export default function FormatDate(props){
     console.log("props ", props)
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"];
+    //let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    //let months = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"];
     if (props.date){
-        if (props.date.getHours() < 10 && props.date.getMinutes() < 10){
-        return <div>{days[props.date.getDay()]}, {props.date.getDate()}{" "}{months[props.date.getMonth()]}<br/>0{props.date.getHours()}:0{props.date.getMinutes()}</div>
+        return <div>
+            <p>{format(props.date, "EEEE, do MMMM")}</p>
+            <p>{format(props.date, "HH:mm")}</p>
+            </div>
     }else{
-        return <div>{days[props.date.getDay()]}, {props.date.getDate()}{" "}{months[props.date.getMonth()]}<br/>{props.date.getHours()}:{props.date.getMinutes()}</div>
-    }}else{
-        return ("Loading...")
+        return "Loading..."
     }
-    //return (props.date ? props.date.getMonth() : "Loading...");
+    
+    //return (props.date ? props.date.getMonth(): "Loading...");
+    //return <p>{days[props.date.getDay()]}, {props.date.getDate()}{" "}{months[props.date.getMonth()]}<br/>{props.date.getHours()<10 ? 0 : ""}{props.date.getHours()}:{props.date.getMinutes() < 10 ? 0 : ""}{props.date.getMinutes()}</p>
+
 }
