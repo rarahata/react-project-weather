a268fd2728f3cc1db2ff7586ba618d48c0844cb8
rarahata <rei.arahata@gmail.com>
2022-01-25 13:06:24 +0000

    move weatherforecast into component

--- src/weatherForecast.js
+++ src/weatherForecast.js
@@ -1,27 +1,14 @@
 import React from "react";
+import "./weatherForecast.css";
+import WeatherIcon from "./weatherIcon";
 
 export default function WeatherForecast(){
     return (
     <div className="WeatherForecast">
     <div className="row">
-    <div className="col-3">
+    <div className="col">
         <p>Tue</p>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon" className="forecast-icon"></img>
-        <p>12°C</p>
-    </div>
-    <div className="col-3">
-        <p>Tue</p>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon" className="forecast-icon"></img>
-        <p>12°C</p>
-    </div>
-    <div className="col-3">
-        <p>Tue</p>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon" className="forecast-icon"></img>
-        <p>12°C</p>
-    </div>
-    <div className="col-3">
-        <p>Tue</p>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-icon" className="forecast-icon"></img>
+        <WeatherIcon code="01d" size={50}/>
         <p>12°C</p>
     </div>
     </div>
